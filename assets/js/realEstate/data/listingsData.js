4d3aff5c7315ec351199b53e55c9ef2c9a18ded8
oliversablove <osablove@gmail.com>
2019-08-21 16:34:34 -0400

    Add Filler Data

---
+++ assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,116 @@
+const data = [
+  {
+    address: '18 Greylock Road',
+    city: 'Brookline',
+    state: 'MA',
+    propertyType: 'mansion',
+    bedrooms: 10,
+    bathrooms: 8,
+    price: 1000000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'swimming pool',
+      'finished basement'
+    ],
+    image: 'https://i.pinimg.com/originals/c2/54/1c/c2541c4354d17b791411753af1a8b959.jpg'
+  },
+  {
+    address: '18 Greylock Road',
+    city: 'Brookline',
+    state: 'MA',
+    propertyType: 'mansion',
+    bedrooms: 10,
+    bathrooms: 8,
+    price: 1000000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'swimming pool',
+      'finished basement'
+    ],
+    image: 'https://patch.com/img/cdn20/users/22926785/20190715/051807/styles/patch_image/public/processed_images/97f367dd52513a01fe128cfcafe01ba2l-m0o.jpg'
+  },
+  {
+    address: '18 Greylock Road',
+    city: 'Brookline',
+    state: 'MA',
+    propertyType: 'mansion',
+    bedrooms: 10,
+    bathrooms: 8,
+    price: 1000000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'swimming pool',
+      'finished basement'
+    ],
+    image: 'https://i.pinimg.com/originals/c2/54/1c/c2541c4354d17b791411753af1a8b959.jpg'
+  },
+  {
+    address: '18 Greylock Road',
+    city: 'Brookline',
+    state: 'MA',
+    propertyType: 'mansion',
+    bedrooms: 10,
+    bathrooms: 8,
+    price: 1000000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'swimming pool',
+      'finished basement'
+    ],
+    image: 'https://i.pinimg.com/originals/f8/e9/bd/f8e9bd96b564f63e47730ac0f249fa8c.jpg'
+  },
+  {
+    address: '18 Greylock Road',
+    city: 'Brookline',
+    state: 'MA',
+    propertyType: 'mansion',
+    bedrooms: 10,
+    bathrooms: 8,
+    price: 1000000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'swimming pool',
+      'finished basement'
+    ],
+    image: 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fkarenhua%2Ffiles%2F2017%2F02%2Fbrady-la2.jpg'
+  },
+  {
+    address: '18 Greylock Road',
+    city: 'Brookline',
+    state: 'MA',
+    propertyType: 'mansion',
+    bedrooms: 10,
+    bathrooms: 8,
+    price: 1000000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'swimming pool',
+      'finished basement'
+    ],
+    image: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/64983743/Giselle_House_1.0.jpg'
+  },
+  {
+    address: '18 Greylock Road',
+    city: 'Brookline',
+    state: 'MA',
+    propertyType: 'mansion',
+    bedrooms: 10,
+    bathrooms: 8,
+    price: 1000000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'swimming pool',
+      'finished basement'
+    ],
+    image: 'https://i.pinimg.com/originals/66/2d/5a/662d5a7247e528b2e7b12788bc5a9cf7.jpg'
+  }
+]
+
+export default data
