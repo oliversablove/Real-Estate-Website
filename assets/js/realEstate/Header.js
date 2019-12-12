893a98e4bbbb9aa6be9cccf06e6936cf44786f75
oliversablove <osablove@gmail.com>
2019-12-12 02:57:33 -0500

    Add site name

--- assets/js/realEstate/Header.js
+++ assets/js/realEstate/Header.js
@@ -12,10 +12,9 @@ class Header extends Component {
   render () {
     return (
       <header>
-        <div className="logo">Logo</div>
+        <div className="logo">Quicklist</div>
 
         <nav>
-          <a href="#">Create Ads</a>
           <a href="#">About Us</a>
           <a href="#">Log In</a>
           <a href="#" className='register-btn'>Register</a>
